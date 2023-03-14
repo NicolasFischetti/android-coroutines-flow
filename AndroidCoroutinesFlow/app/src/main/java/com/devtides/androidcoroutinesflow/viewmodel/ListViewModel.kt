f974685f5532f363745dbe72d2df17172190b71d
Nicolas Fischetti <nicolas.a.fischetti@gmail.com>
2023-03-14 17:54:53 -0300

    Android coroutines flow

---
+++ AndroidCoroutinesFlow/app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -0,0 +1,10 @@
+package com.devtides.androidcoroutinesflow.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
+
+class ListViewModel: ViewModel() {
+
+    val newsArticles = NewsRepository().getNewsArticles().asLiveData()
+}
